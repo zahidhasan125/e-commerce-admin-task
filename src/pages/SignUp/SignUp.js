8cc34cc9a55d22c6a4aa793ae57db7c2a5ade356
zahidhasan125 <zahidhasan125@gmail.com>
2023-01-25 04:47:59 +0600

    Signup and login ui complete

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const SignUp = () => {
     return (
         <div className="hero my-10">
             <div className="hero-content flex-col lg:flex-row-reverse w-full md:w-3/5 lg:max-w-sm">
                 <div className="card shadow-2xl w-full border-t">
-                    <h2 className='text-4xl text-center font-bold mt-4 text-white'>Login</h2>
+                    <h2 className='text-4xl text-center font-bold mt-4 text-white'>Sign Up</h2>
                     <form className="card-body pt-0">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-white">Full Name</span>
+                            </label>
+                            <input type="text" placeholder="John Doe" className="input input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-white">Email</span>
@@ -21,10 +27,10 @@ const Login = () => {
                             <input type="password" placeholder="P@$$w0Rd" className="input input-bordered" />
                         </div>
                         <label className="label">
-                            <span className="label-text-alt text-white">New to E-Shop? <Link to="/signup" className='link link-hover text-md text-cyan-500 font-bold'>Sign Up</Link></span>
+                            <span className="label-text-alt text-white">Already have an account? <Link to="/login" className='link link-hover text-md text-cyan-500 font-bold'>Login</Link></span>
                         </label>
                         <div className="form-control mt-4">
-                            <button className="btn btn-outline text-white">Login</button>
+                            <button className="btn btn-outline text-white">Register</button>
                         </div>
                     </form>
                 </div>
@@ -33,4 +39,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../layouts/Main';
 import Home from '../pages/Home/Home';
 import Login from '../pages/Login/Login';
+import SignUp from '../pages/SignUp/SignUp';
 
 export const router = createBrowserRouter(
     [
@@ -17,6 +18,10 @@ export const router = createBrowserRouter(
                 {
                     path: '/login',
                     element: <Login />
+                },
+                {
+                    path: '/signup',
+                    element: <SignUp />
                 }
             ]
         }
