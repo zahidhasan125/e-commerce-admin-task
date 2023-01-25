565285d5492d138071bf807e8ad523451327f9d1
zahidhasan125 <zahidhasan125@gmail.com>
2023-01-26 00:16:12 +0600

    firebase config added, private route created

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,49 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { app } from '../../firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+export const AuthContext = createContext();
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    const logout = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser === null || currentUser.emailVerified || currentUser.providerData[0].uid) {
+                setUser(currentUser);
+            }
+            setLoading(false);
+        })
+        return () => unsubscribe();
+    }, [])
+
+    const authValue = {
+        user,
+        loading,
+        createUser,
+        login,
+        logout
+    }
+    return (
+        <AuthContext.Provider value={authValue}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className='text-center my-8'>
+        <div className='text-center my-8 text-white'>
             <h1 className='text-4xl'>Hello Admin</h1>
             <h3 className='text-3xl font-bold'>Welcome to Admin Panel</h3>
             <p className='text-lg'>Please <Link to="/login" className='underline'>Login</Link> to access your Dashboard</p>
