f016befdc5e73856dd4ffff2d7ffe7025252a8eb
zahidhasan125 <zahidhasan125@gmail.com>
2023-01-25 15:55:44 +0600

    starting private route

--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const menuItems = <>
+        <li><Link>Products</Link></li>
+        <li><Link>Cart</Link></li>
+        <li><Link>Orders</Link></li>
+        <li><Link>Customers</Link></li>
+    </>
     return (
         <div className="navbar text-white">
             <div className="navbar-start">
@@ -10,36 +16,18 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link>Item 1</Link></li>
-                        <li tabIndex={0}>
-                            <Link className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </Link>
-                            <ul className="p-2 bg-base-100">
-                                <li><Link>Submenu 1</Link></li>
-                                <li><Link>Submenu 2</Link></li>
-                            </ul>
-                        </li>
-                        <li><Link>Item 3</Link></li>
+                        {
+                            menuItems
+                        }
                     </ul>
                 </div>
                 <Link to='/' className="btn btn-ghost normal-case text-2xl font-bold">E-SHOP</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link>Item 1</Link></li>
-                    <li tabIndex={0}>
-                        <Link>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </Link>
-                        <ul className="p-2">
-                            <li><Link>Submenu 1</Link></li>
-                            <li><Link>Submenu 2</Link></li>
-                        </ul>
-                    </li>
-                    <li><Link>Item 3</Link></li>
+                    {
+                        menuItems
+                    }
                 </ul>
             </div>
             <div className="navbar-end">
---
+++ src/pages/Products/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            Your Product will show here
+        </div>
+    );
+};
+
+export default Products;
